275ff9097c025bf4081550afb18dcf2e1a9a21db
Ashesh Maharjan <asheshmhrzn21@gmail.com>
2024-01-25 14:22:48 +0545

    categorycaontroller done

--- src/main/java/com/blog/example/services/Impl/CategoryServiceImpl.java
+++ src/main/java/com/blog/example/services/Impl/CategoryServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.blog.example.entities.Category;
 import com.blog.example.exceptions.ResourceNotFoundException;
@@ -12,6 +13,7 @@ import com.blog.example.payloads.CategoryDto;
 import com.blog.example.repositories.CategoryRepo;
 import com.blog.example.services.CategoryService;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
 	@Autowired
