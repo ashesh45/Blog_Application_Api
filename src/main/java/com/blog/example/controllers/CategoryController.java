9088d86a449b90db58ff01d69f1bb562bd5ab4ba
Ashesh Maharjan <asheshmhrzn21@gmail.com>
2024-01-28 12:30:11 +0545

    mapped

--- src/main/java/com/blog/example/controllers/CategoryController.java
+++ src/main/java/com/blog/example/controllers/CategoryController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,7 +28,7 @@ public class CategoryController {
 
 	//create
 	@PostMapping("/")
-	public ResponseEntity<CategoryDto> createCategory(@RequestBody CategoryDto categoryDto)
+	public ResponseEntity<CategoryDto> createCategory(@RequestBody  CategoryDto categoryDto)
 	{
 	
 		CategoryDto createCategory = this.categoryService.createCategory(categoryDto);
--- src/main/java/com/blog/example/payloads/CategoryDto.java
+++ src/main/java/com/blog/example/payloads/CategoryDto.java
@@ -1,14 +1,17 @@
 package com.blog.example.payloads;
 
+
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class CategoryDto {
-	
+
 	private int CategoryId;
 	private String categoryTitle;
 	private String categoryDescription;
